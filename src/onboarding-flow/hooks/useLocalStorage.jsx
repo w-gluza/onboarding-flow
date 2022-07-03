31a86d0aced328781ba53821e43a1760f5cffee4
w-gluza <wiolagluza@gmail.com>
2022-07-03 15:48:40 +0200

    fix: unused variables

--- src/onboarding-flow/hooks/useLocalStorage.jsx
+++ src/onboarding-flow/hooks/useLocalStorage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const useLocalStorage = (storageKey, fallbackState) => {
   const [value, setValue] = useState(JSON.parse(localStorage.getItem(storageKey)) ?? fallbackState);
