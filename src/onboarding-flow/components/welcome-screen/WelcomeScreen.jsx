0e74c4003441d4fda0cee811623ddcae85d9c219
w-gluza <wiolagluza@gmail.com>
2022-07-04 17:54:34 +0200

    welcome screen wip

--- src/onboarding-flow/components/welcome-screen/WelcomeScreen.jsx
+++ src/onboarding-flow/components/welcome-screen/WelcomeScreen.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import "./WelcomeScreen.css";
 
 function WelcomeScreen() {
-  return <div>WelcomeScreen</div>;
+  return (
+    <div className="welcome-screen">
+      <p>KYC & Due Dilligence</p>
+      <h1>Add new customer</h1>
+      <p>
+        Estimated time: <span className="text--bold">10 minutes</span>
+      </p>
+    </div>
+  );
 }
 
 export default WelcomeScreen;
